773f743ad1ce44ea2b84a3fe683ec4e72fb0bc1d
Shaundator <rash0081@kea.stud.dk>
2021-09-23 12:36:39 +0200

    Version 1.02

--- src/Tests/Triangle.java
+++ src/Tests/Triangle.java
@@ -12,65 +12,30 @@ public class Triangle {
         this.c=c;
     }
     public String getType(){
-        if (!validCheck(a, b, c)){
+        if(!validCheck(a, b, c)){
             return "ikke gyldig";
         }
         if((a==b) && (b==c)){
             return "Ligesidet";
         }
-        if((a==b)&&(b!=c) || (a!=b)&&(b==c) || (a==c)&&(b!=c)){
+        if((a==b)||(b==c)||(a==c)){
             return "ligebenet";
         }
-        if((a!=b&&b!=c)){
-            return "Scalene";
-        }
-        return "Skejs";
+        return "Scalene";
     }
     public void triangleType(){
         System.out.println("Trekanten er " + getType());
     }
     public boolean validCheck(int a, int b, int c) {
-        int tempTest;
-        int temp1;
-        int temp2;
-        if (a==b&&b==c){
-            return true;
+        if((a>=b+c)||(b>=a+c)||(c>=a+b)){
+            return false;
         }
-        else if (a == b) {
-            tempTest = a+b;
-            temp1 = Math.max(tempTest, c);
-            temp2 = Math.min(tempTest, c);
-        }else if (a == c) {
-            tempTest = a+c;
-            temp1 = Math.max(tempTest, b);
-            temp2 = Math.min(tempTest, b);
-        }else if (b == c) {
-            tempTest = b+c;
-            temp1 = Math.max(tempTest, a);
-            temp2 = Math.min(tempTest, a);
-        }else {
-            tempTest = Math.max(a,b);
-            temp1 = Math.max(tempTest, c);
-            temp2 = Math.min(tempTest, c) + Math.min(a,b);
-        }
-        if (temp1 >= temp2) {
-            return false;//invalid
-        } else {
-            return true;//valid
-        }
+        return true;
     }
-
     public String toString(){
         return "Side a: " + a +
                 "\nSide b: " + b +
                 "\nSide c: " + c +
                 "\nTrekanten er " + getType();
     }
-
-
-
-
-
-
-
 }
